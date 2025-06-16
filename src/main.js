9f1cb91e65c269bea4915f150b3901a55e533732
Jiri Zak <jirizakx@gmail.com>
2025-06-16 12:05:53 +0200

    Upload to github

---
+++ src/main.js
@@ -0,0 +1,19 @@
+// header:
+document.getElementById('header').innerHTML = `
+  <img id="icon" src="/mt/imgs/icon.png" alt="">
+  <a href="https://zak.software/mt" id="mt-link">Minimalist travel</a>
+  <section id="links">
+    <a id="tips-link" href="https://zak.software/mt/tips">Tips and Tricks</a>
+    <a id="resources-link" href="https://zak.software/mt/resources">Resources</a>
+    <a id="forum-link">Forum</a>
+    <a id="about-link" href="https://zak.software/mt/about">About</a>
+  </section>
+`
+
+// footer:
+document.getElementById('footer').innerHTML = `
+  <section id="footer-content">
+    Jiri Zak design, more on <a href="https://zak.software" class="paragraph-ref">zak.software</a>
+  </section>
+`
+
